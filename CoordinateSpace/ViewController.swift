faaa17bff898660a76dbe3ce5605ccce43f8e3ea
joesus <joesusnick@gmail.com>
2017-05-09 14:18:03 -0600

    Initial Commit

---
+++ CoordinateSpace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoordinateSpace
+//
+//  Created by Joesus on 5/9/17.
+//  Copyright © 2017 Joesus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
